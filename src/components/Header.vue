0c24252dfc20c59b851ca4a4eddcf4959c9fcec2
HZcoding123 <975955434@qq.com>
2022-05-09 21:49:08 +0800

    登录注册功能完成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,35 +22,50 @@
           </div>
           <a class="glyphicon glyphicon-search" aria-hidden="true"></a>
         </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
-            <ul class="dropdown-menu">
-              <li><a @click="put_login"><el-icon style="font-size: 18px;top: 2px"><avatar /></el-icon> 账号登录</a></li>
-              <li><a href="#"><el-icon style="font-size: 18px;top: 2px"><chat-dot-round/></el-icon> 微信登录</a></li>
-              <li><a href="#"><el-icon style="font-size: 18px;top: 3px"><chat-dot-square /></el-icon> QQ登录</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#"><el-icon style="font-size: 18px;top: 2px"><edit /></el-icon> 注册</a></li>
+            <img :src="icon" class="icon_1"  v-if="username">
+            <ul class="nav navbar-nav navbar-right" v-else>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a @click="put_login"><el-icon style="font-size: 18px;top: 2px"><avatar /></el-icon> 账号登录</a></li>
+                  <li><a href="#"><el-icon style="font-size: 18px;top: 2px"><chat-dot-round/></el-icon> 微信登录</a></li>
+                  <li><a href="#"><el-icon style="font-size: 18px;top: 3px"><chat-dot-square /></el-icon> QQ登录</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a @click="put_register"><el-icon style="font-size: 18px;top: 2px"><edit /></el-icon> 注册</a></li>
+                </ul>
+              </li>
             </ul>
-          </li>
-        </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
-  <Login v-if="is_login" @close="close_login"/>
+  <Login v-if="is_login" @close="close_login" @loginsuccess="login_success" @change_login_register="change_login_register"/>
+  <Register v-if="is_register" @close="close_register" @change_login_register="change_login_register" @loginsuccess="login_success"/>
 </template>
 
 <script>
 import Login from "./Login";
+import Register from "./Register";
 export default {
   name: "Header",
-  components: {Login},
+  components: {
+    Login,
+    Register,
+  },
   data() {
     return {
       url_path: sessionStorage.url_path || '/',
       isClick: false,
       is_login: false,
+      is_register: false,
+      username: '',
+      token: '',
+      icon: '',
     }
+  },
+  created() {
+    this.username = this.$cookies.get('username');
+    this.token = this.$cookies.get('token');
+    this.icon = this.$cookies.get('icon');
   },
   methods: {
     close_login() {
@@ -58,6 +73,28 @@ export default {
     },
     put_login() {
       this.is_login = true;
+    },
+    login_success() {
+      this.username = this.$cookies.get('username');
+      this.token = this.$cookies.get('token');
+      this.icon = this.$cookies.get('icon');
+    },
+    put_register() {
+      this.is_register = true;
+    },
+    close_register() {
+      this.is_register = false;
+    },
+    change_login_register() {
+      if(this.is_login) {
+        this.is_login = false;
+        this.is_register = true;
+        return
+      }
+      if(!this.is_login) {
+        this.is_register = false;
+        this.is_login = true;
+      }
     }
   }
 }
@@ -91,5 +128,13 @@ export default {
 
 .glyphicon-user{
   margin-left: 0px;
+}
+
+.icon_1{
+  float: right;
+  margin-top: 5px;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
 }
 </style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,29 +1,19 @@
 <template>
-  <div class="login">
+  <div class="register">
     <div class="popup-content">
       <div class="close-btn">
-        <el-icon style="color: white; font-size: 20px" @click="close_login"><close-bold /></el-icon>
+        <el-icon style="color: white; font-size: 20px" @click="close_register"><close-bold /></el-icon>
       </div>
       <div class="signin">
-        <ul>
-          <li :class="{active: login_method === 'is_pwd'}" @click="change_login_methods('is_pwd')">密码登录</li>
-          <li :class="{active: login_method === 'is_sms'}" @click="change_login_methods('is_sms')">短信登录</li>
-        </ul>
-        <div v-if="login_method === 'is_pwd'">
-          <div class="username inputcontain">
+        <span class="title">新用户注册</span>
+        <div>
+          <div class="mobile inputcontain">
             <img src="//hcdn2.luffycity.com/media/frontend/index/shouji@1x_1560849098.2282765.svg" alt="">
-            <input type="text" maxlength="20" autocomplete="off" placeholder="用户名/手机号/邮箱" v-model="username">
+            <input type="text" maxlength="20" placeholder="手机号" v-model="mobile" @blur="check_mobile">
           </div>
           <div class="password inputcontain">
             <img src="//hcdn2.luffycity.com/media/frontend/index/mima-4@1x_1560849097.9023619.svg" alt="">
             <input maxlength="20" autocomplete="off" placeholder="密码" type="password" v-model="password">
-          </div>
-          <button @click="login_pwd">登录</button>
-        </div>
-        <div v-if="login_method === 'is_sms'">
-          <div class="mobile inputcontain">
-            <img src="//hcdn2.luffycity.com/media/frontend/index/shouji@1x_1560849098.2282765.svg" alt="">
-            <input type="text" maxlength="20" placeholder="手机号" v-model="mobile" @blur="check_mobile">
           </div>
           <div class="checkword inputcontain">
             <img src="//hcdn2.luffycity.com/media/frontend/index/yanzhengma-7@1x_1560849098.1486425.svg" alt="">
@@ -31,10 +21,10 @@
             <span class="inline">|</span>
             <span class="link-btn" @click="send_sms">{{sms_interval}}</span>
           </div>
-          <button @click="login_sms">登录</button>
+          <button @click="register">注册</button>
         </div>
         <div class="other-way">
-          <div @click="change_login_register"><span class="link-btn">立即注册</span></div>
+          <div @click="change_login_register"><span class="link-btn">立即登录</span></div>
         </div>
       </div>
     </div>
@@ -45,25 +35,40 @@
 import axios from "axios";
 
 export default {
-  name: "Login",
-  data() {
-    return {
-      login_method: 'is_pwd',
-      username: '',
+  name: "Register",
+  data(){
+    return{
+      sms_interval:'获取验证码',
+      mobile: '',
       password: '',
       sms: '',
-      mobile:'',
-      sms_interval: '获取验证码',
       is_send: false,
-      is_login: false
     }
   },
-  methods: {
-    close_login() {
+  created() {
+  },
+  methods:{
+    close_register(){
       this.$emit('close')
     },
-    change_login_methods(method) {
-      this.login_method = method;
+    change_login_register() {
+      this.$emit('change_login_register')
+    },
+    check_mobile() {
+      if(!this.mobile){
+        return
+      }
+      if(!this.mobile.match(/^1[3-9][0-9]{9}$/)) {
+        this.$message({
+          message: '手机号有误',
+          type: 'warning',
+          duration: 1000,
+          onClose:()=>{
+            this.mobile = '';
+          }
+        })
+      }
+      this.is_send = true;
     },
     send_sms() {
       if(!this.mobile){
@@ -71,129 +76,78 @@ export default {
           message: '手机号不能为空',
           type: 'warning',
           duration: 1000,
-        });
+        })
         return
       }
       if(!this.is_send){
         return
-      };
-      // 判断手机不能为空
-      this.is_send = false;
+      }
+      this.is_send = false
       axios.get(this.$settings.base_url + '/user/check/',{
-        params: {
+        params:{
           'telephone': this.mobile
         }
       }).then(response=>{
-        if(response.data.code === 100){
-          let sms_interval_time = 60;
-          this.sms_interval = '发送中...';
-          let timer = setInterval(() => {
-            if (sms_interval_time <= 1) {
-              clearInterval(timer);
-              this.sms_interval = "获取验证码";
-              this.is_send = true; // 重新回复点击发送功能的条件
+        if(response.data.code === 0){
+          let sms_interval_time = 60
+          this.sms_interval = '发送中...'
+          let timer = setInterval(()=>{
+            if(sms_interval_time <= 1){
+              clearInterval(timer)
+              this.sms_interval = '获取验证码'
+              this.is_send = true
             } else {
-              sms_interval_time -= 1;
-              this.sms_interval = `${sms_interval_time}s`;
+              sms_interval_time -= 1
+              this.sms_interval = `${sms_interval_time}s`
             }
           }, 1000);
-          axios.get(this.$settings.base_url + '/user/send/',
-              {
-                      params: {
-                        'telephone': this.mobile
-                      }
-              }).then(response=>{
-          })
-        }else{
-          this.$message({
-            message: '手机号不存在',
-            type: 'warning',
-            duration: 1000,
-            onClose: ()=>{
-              this.mobile = '';
+          axios.get(this.$settings.base_url + '/user/send/',{
+            params: {
+              'telephone': this.mobile
             }
+          }).then(response=>{
           });
-        }
-      });
-    },
-    check_mobile() {
-      // 当失去焦点后，会字自动执行这个方法
-      if (!this.mobile){
-        return
-      }
-      if (!this.mobile.match(/^1[3-9][0-9]{9}$/)){
-        this.is_login = false;
-        this.$message({
-          message: '手机号有误',
-          type: 'warning',
-          duration: 1000,
-          onClose:()=>{
-          this.mobile = '';
-        }
-        });
-        return false
-      }
-      this.is_send = true;
-      this.is_login = true;
-    },
-    login_pwd() {
-      if(!this.mobile || !this.password){
-        this.$message({
-          message: '账号密码不能为空',
-          type: 'warning',
-          duration: 1000
-        })
-        return
-      }
-      axios.post(this.$settings.base_url + '/user/login/', {
-        username: this.username,
-        password: this.password,
-      }).then(response=> {
-        if(response.data.code === 100){
-          this.$cookies.set('username', response.data.username, '7d')
-          this.$cookies.set('token', response.data.token, '7d')
-          let icon = this.$settings.base_url + '/media/' + response.data.icon + '/'
-          this.$cookies.set('icon', icon, '7d')
-          this.close_login()
-          // 给父组件head传递一个事件，让它从cookie中取出来token和username
-          this.$emit('loginsuccess')
-        }
-        if(response.data.code === 0){
+        } else {
           this.$message({
-            message: response.data.result['non_field_errors'][0],
+            message: '该手机号已注册',
             type: 'warning',
             duration: 1000,
             onClose:()=>{
-              this.username = '';
-              this.password = '';
+              this.mobile = ''
             }
           })
         }
-          });
+      })
     },
-    login_sms() {
-      if(!this.sms){
-        this.$message({
-          message: '验证码不能为空',
-          type: 'warning',
-          duration: 1000,
-        });
-        return false
-      }
-
+    register() {
+      // 点击注册
       if(!this.mobile){
         this.$message({
           message: '手机号不能为空',
           type: 'warning',
           duration: 1000,
         });
-        return false
+        return;
       }
-      if(!this.is_login){
-        return
+      if(!this.password){
+        this.$message({
+          message: '密码不能为空',
+          type: 'warning',
+          duration: 1000,
+        });
+        return;
       }
-      axios.post(this.$settings.base_url + '/user/code/', {
+      if(!this.sms){
+        this.$message({
+          message: '验证码不能为空',
+          type: 'warning',
+          duration: 1000,
+        });
+        return;
+      }
+      axios.post(this.$settings.base_url + '/user/register/',{
         telephone: this.mobile,
+        password: this.password,
         code: this.sms,
       }).then(response=>{
         if(response.data.code === 100){
@@ -203,29 +157,26 @@ export default {
           this.$cookies.set('icon', icon, '7d')
           this.close_login()
           this.$emit('loginsuccess')
-        }
-        if(response.data.code === 0){
+        } else {
           this.$message({
             message: response.data.result['non_field_errors'][0],
             type: 'warning',
             duration: 1000,
             onClose:()=>{
-              this.mobile = '';
-              this.sms = '';
+              this.mobile = ''
+              this.sms = ''
+              this.password = ''
             }
           })
         }
       });
-    },
-    change_login_register() {
-      this.$emit('change_login_register')
     }
   }
 }
 </script>
 
 <style scoped>
-.login{
+.register{
   height: 100%;
   position: fixed;
   left: 0;
@@ -250,6 +201,11 @@ export default {
   position: relative;
 }
 
+.title{
+  color: #ffc210;
+  font-size: 17px;
+}
+
 .close-btn{
   width: 26px;
   height: 26px;
@@ -270,30 +226,21 @@ export default {
   justify-content: center;
 }
 
- ol, ul {
-   margin: 0;
-   padding: 0;
-   list-style: none;
- }
-
-.popup-content .signin ul .active{
-  color: #ffc210;
-  border-bottom: 1px solid #ffc210;
-}
-
-.input_active{
-  border: 1px solid #ffc210;
+ol, ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 
 .popup-content .signin ul li{
-   width: 50%;
-   font-size: 16px;
-   font-weight: 500;
-   color: #4a4a4a;
-   cursor: pointer;
-   padding-bottom: 20px;
-   border-bottom: 1px solid #e9e9e9;
- }
+  width: 50%;
+  font-size: 16px;
+  font-weight: 500;
+  color: #4a4a4a;
+  cursor: pointer;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e9e9e9;
+}
 
 li {
   list-style: none;
@@ -302,15 +249,6 @@ li {
 div, h1, h2, h3, h4, h5, h6, p, section, span {
   -webkit-margin-before: 0;
   -webkit-margin-after: 0;
-}
-
-.popup-content .signin .username{
-  border: 1px solid #ddd;
-  padding-left: 15px;
-  display: flex;
-  align-items: center;
-  margin-top: 40px;
-  position: relative;
 }
 
 .popup-content .signin .username img{
@@ -351,15 +289,6 @@ button, input, textarea {
 input, select, textarea {
   border: none;
   outline: none;
-}
-
-.popup-content .signin .password{
-  border: 1px solid #ddd;
-  padding-left: 15px;
-  display: flex;
-  align-items: center;
-  margin-top: 40px;
-  position: relative;
 }
 
 .popup-content .signin .password img{
@@ -411,8 +340,8 @@ button {
 }
 
 .popup-content .signin .link-btn{
-   color: #ffc220!important;
-   cursor: pointer;
+  color: #ffc220!important;
+  cursor: pointer;
 }
 
 .popup-content .signin .mobile{
@@ -458,5 +387,26 @@ button {
   font-weight: 400;
   color: #d8d8d8;
   margin-right: 15px;
+}
+
+.popup-content .signin .password{
+  border: 1px solid #ddd;
+  padding-left: 15px;
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  position: relative;
+}
+
+.popup-content .signin .password img{
+  width: 14px;
+}
+
+.popup-content .signin .password input{
+  flex: 1;
+  margin-left: 12px;
+  font-size: 14px;
+  height: 34px;
+  border: none;
 }
 </style>
